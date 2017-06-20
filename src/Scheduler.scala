22869b78cbe9e24eec573b5c7fc770da0bbfe5e2
Yaopeng Wu <yaopeng.wu@chartboost.com>
2017-06-20 12:05:35 -0700

    make take to execute in worker-thread not the caller

--- src/Scheduler.scala
+++ src/Scheduler.scala
@@ -1,6 +1,6 @@
-import java.util.concurrent.PriorityBlockingQueue
+import java.util.concurrent.{Executors, PriorityBlockingQueue}
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 
 //type Task = () => Unit
 
@@ -9,18 +9,28 @@ trait Scheduler {
   def start()(implicit ec: ExecutionContext): Unit
 }
 
-
-class SchedulerImp(val minHeap: PriorityBlockingQueue[ScheduledTask]) extends Scheduler {
+class SchedulerImp(
+    val minHeap: PriorityBlockingQueue[ScheduledTask],
+    // ExecutionContext for Future to execute a task in worker thread (not calling thread)
+    implicit val ec: ExecutionContext = ExecutionContext.fromExecutorService(Executors.newFixedThreadPool(5))
+) extends Scheduler {
 
   override def schedule(task: () => Unit, timestamp: Long): Unit = {
     minHeap.offer(ScheduledTask(task, timestamp))
   }
 
   override def start()(implicit ec: ExecutionContext): Unit = {
-    while(true) {
+    while (true) {
       // put and take are blocking
       val nextTask = minHeap.take()
-      if (nextTask.timestamp < System.currentTimeMillis) nextTask.task() else minHeap.put(nextTask)
+      if (nextTask.timestamp < System.currentTimeMillis) {
+        Future {
+          nextTask.task()
+        }
+      } else {
+        minHeap.put(nextTask)
+        Thread.sleep(100)
+      }
     }
   }
 }
