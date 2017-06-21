2b49303a768bd5f147d9e84a6a1ef13d2f1d82a1
Yaopeng Wu <yaopeng.wu@chartboost.com>
2017-06-21 12:33:55 -0700

    avoid busy checking the queue

--- src/main/scala/SchedulerImp.scala
+++ src/main/scala/SchedulerImp.scala
@@ -8,26 +8,34 @@ class SchedulerImp(
 
   @volatile var running: Boolean = false
 
+  val taskRunner = new {
+    def run()(implicit ec: ExecutionContext): Unit = {
+      while (running) {
+        // put and take are blocking
+        val nextTask = minHeap.take()
+        val executionTime = nextTask.timestamp
+        if (executionTime < System.currentTimeMillis) {
+          Future {
+            nextTask.task()
+          }
+        } else {
+          minHeap.put(nextTask)
+          this.wait(executionTime)
+        }
+      }
+    }
+  }
+
   override def schedule(task: () => Unit, timestamp: Long): Unit = {
     minHeap.offer(ScheduledTask(task, timestamp))
+    taskRunner.notify()
   }
 
   override def start()(implicit ec: ExecutionContext): Unit = {
     if (!running) {
       running = true
       Future {
-        while (running) {
-          // put and take are blocking
-          val nextTask = minHeap.take()
-          if (nextTask.timestamp < System.currentTimeMillis) {
-            Future {
-              nextTask.task()
-            }
-          } else {
-            minHeap.put(nextTask)
-            Thread.sleep(100)
-          }
-        }
+        taskRunner.run()
       }
     }
   }
