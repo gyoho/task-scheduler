8c736de6758bafc158011cb04c0bf0947c4b236b
Yaopeng Wu <yaopeng.wu@chartboost.com>
2017-06-20 21:00:44 -0700

    fixed bug

--- src/main/scala/Scheduler.scala
+++ src/main/scala/Scheduler.scala
@@ -1,7 +1,7 @@
 import scala.concurrent.ExecutionContext
 
 trait Scheduler {
-  def schedule(task: => Unit, timestamp: Long): Unit
+  def schedule(task: () => Unit, timestamp: Long): Unit
   def start()(implicit ec: ExecutionContext): Unit
   def stop(): Unit
 }
